ceca8d373ecb5f091d0347bd5dab6cb017d4c7cd
mawhinn <ajmawhinn@gmail.com>
2018-07-10 11:39:54 -0500

    Responsive Title fix

--- support_files/support.js
+++ support_files/support.js
@@ -1,17 +1,17 @@
 /* Javascript */
     $(window).scroll(function() {
         var scrollviz = $(window).scrollTop();
-        if (scrollviz > window.innerHeight - 600) {
+        if (scrollviz > window.innerHeight - 300) {
             $(".back1").removeClass("back2");
         } else {
             $(".back1").addClass("back2");
         }
     });
-    
-    
+
+
     $(window).scroll(function() {
         var scrollviz = $(window).scrollTop();
-        if (scrollviz > window.innerHeight - 600) {
+        if (scrollviz > window.innerHeight - 300) {
             $(".font-color1").removeClass("font-color2");
         } else {
             $(".font-color1").addClass("font-color2");
